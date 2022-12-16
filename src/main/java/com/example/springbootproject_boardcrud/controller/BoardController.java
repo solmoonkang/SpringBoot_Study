61ff32fb52e3ad24e6722d6227808627229499f2
solmoonkang <thfans0521@naver.com>
2022-12-17 04:22:53 +0900

    2022.12.17 -> 각 구현회면에 해당하는 어노테이션에 대한 의미 및 Controller, Service 게시글 상세조회 및 수정 페이지, 게시글 수정 및 삭제

--- src/main/java/com/example/springbootproject_boardcrud/controller/BoardController.java
+++ src/main/java/com/example/springbootproject_boardcrud/controller/BoardController.java
@@ -1,12 +1,12 @@
 package com.example.springbootproject_boardcrud.controller;
 
+import com.example.springbootproject_boardcrud.domain.repository.BoardRepository;
 import com.example.springbootproject_boardcrud.dto.BoardDto;
 import com.example.springbootproject_boardcrud.service.BoardService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +15,7 @@ import java.util.List;
 // 그래서 BoardService 객체를 주입 받을 때 @Autowired 같은 특별한 어노테이션을 부여하지 않음
 public class BoardController {
     private BoardService boardService;
+    private final BoardRepository boardRepository;
 
     // 게시글 목록
     // public String list(Model model) {} -> Model 객체를 통해 View에 데이터를 전달
@@ -35,6 +36,40 @@ public class BoardController {
     public String write(BoardDto boardDto) {
         boardService.savePost(boardDto);
         // dto : Controller와 Service 사이에서 데이터를 주고받는 객체를 의미
+        return "redirect";
+    }
+
+    // TODO : 게시글 상세조회 및 수정 페이지, 게시글 수정 및 삭제
+    @GetMapping("/post/{no}")
+    public String detail(@PathVariable("no") Long no, Model model) {
+        // 유동적으로 변하는 PathVariable을 처리하는 방법
+        // URL 매핑하는 부분에서 {변수} 처리를 해주면, 메소드 파라미터로 @PathVariable("변수")의 형식으로 받을 수 있다
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+        return "board/detail.html";
+    }
+
+    @GetMapping("/post/edit/{no}")
+    public String edit(@PathVariable("no") Long no, Model model) {
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+        return "board/update.html";
+    }
+
+    @PutMapping("/post/edit/{no}")
+    public String update(BoardDto boardDto) {
+        // update() -> 게시글 추가에서 사용하는 boardService.savePost() 메소드를 같이 사용하고 있다
+        boardService.savePost(boardDto);
+
+        return "redirect";
+    }
+
+    @DeleteMapping("/post/{no}")
+    public String delete(@PathVariable("no") Long no) {
+        boardService.deletePost(no);
+
         return "redirect";
     }
 }
--- src/main/java/com/example/springbootproject_boardcrud/service/BoardService.java
+++ src/main/java/com/example/springbootproject_boardcrud/service/BoardService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service               // 서비스 계층임을 명시해주는 어노테이션
 @AllArgsConstructor    // Controller에서 봤던 어노테이션으로 Repository를 주입하기 위해 사용
@@ -23,21 +24,47 @@ public class BoardService {
         List<BoardDto> boardDtoList = new ArrayList<>();
 
         for (BoardEntity boardEntity : boardEntities) {
-            BoardDto boardDTO = BoardDto.builder()
+            BoardDto boardDto = BoardDto.builder()
                     .id(boardEntity.getId())
                     .writer(boardEntity.getWriter())
                     .title(boardEntity.getTitle())
                     .content(boardEntity.getContent())
                     .createdDate(boardEntity.getCreatedDate())
                     .build();
-            boardDtoList.add(boardDTO);
+            boardDtoList.add(boardDto);
         }
 
         return boardDtoList;
     }
 
+    @Transactional
+    public BoardDto getPost(Long id) {
+        // findById() :
+            // PK 값을 where 조건으로 하여 데이터를 가져오기 위한 메소드이며, JpaRepository 인터페이스에 정의되어 있다
+            // 반환 값은 Optional 타입인데, 엔티티를 쏙 빼오려면 boardEntityWrapper.get(); 이렇게 get() 메소드를 사용해 가져온다
+        Optional<BoardEntity> boardEntityWrapper = boardRepository.findById(id);
+        BoardEntity boardEntity = boardEntityWrapper.get();
+
+        BoardDto boardDto = BoardDto.builder()
+                .id(boardEntity.getId())
+                .writer(boardEntity.getWriter())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getContent())
+                .createdDate(boardEntity.getCreatedDate())
+                .build();
+
+        return boardDto;
+    }
+
     @Transactional      // 선언적 트랜잭션이라 부르며, 트랜잭션을 적용하는 어노테이션
     public Long savePost(BoardDto boardDto) {
         return boardRepository.save(boardDto.toEntity()).getId();   // JpaRepository에서 정의된 메소드로, DB에 INSERT, UPDATE를 담당함 -> 매개변수로는 Entity를 전달
+    }
+
+    @Transactional
+    public void deletePost(Long id) {
+        // deleteById() :
+            // PK 값을 where 조건으로 하여 데이터를 삭제하기 위한 메소드이며, JpaRepository 인터페이스에 정의되어 있다
+        boardRepository.deleteById(id);
     }
 }
